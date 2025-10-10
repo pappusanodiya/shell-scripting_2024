1c0c24d2597f6f3ea4579e793acb925cd8fee714
Pappu sanodiya <81160267+pappusanodiya@users.noreply.github.com>
2025-10-10 15:07:53 +0530

    Create LockUserBackupCreate.sh

---
+++ LockUserBackupCreate.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+read -p "enter ur username to lock and backup : " user
+
+if [ -z $user ]
+then
+	echo "username is not provided
+else
+	if id $user &> /dev/null
+	then
+		passwd -l $user
+		homeDir=$(grep ^${user}: /etc/passwd | cut -d ":" -f 6
+		echo $homeDir
+		if [ -d $homeDir ]
+		then 	
+			echo "home dir exists..."
+			tar -cf ${user}.tar $homeDir
+		fi
+		exit 0
+	else
+		echo "provided user $user not exists.."
+
+	fi
+fi
